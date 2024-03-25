9311981f939cda4c5d66ce343c48c559fa7a9c5f
younus amshan <younusamshankhan@gmail.com>
2024-03-25 05:54:27 +0500

    Table

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+var userInput2 = prompt('Wich Table do You Creat Enter Your Table Number')
+var userInput = prompt('How Long Table Do You Creat')
+
+
+
+for(var i = 1; i <= userInput; i++){
+document.write( userInput2 + ' x ' + i + ' = ' + (userInput2 * i)  + ' <br> ')
+}
